133f98328f41bf66e3dafbbfefa9e46042666bf7
yangwang166 <yangwang166>
2024-04-22 14:44:31 +0000

    Init Commit

---
+++ DDL CICD PoC.sql
@@ -0,0 +1,30 @@
+-- Databricks notebook source
+create catalog if not exists lenovo
+
+-- COMMAND ----------
+
+use catalog lenovo
+
+-- COMMAND ----------
+
+create schema if not exists poc
+
+-- COMMAND ----------
+
+use schema poc
+
+-- COMMAND ----------
+
+CREATE TABLE IF NOT EXISTS sample_table (
+  id INT,
+  name VARCHAR(50),
+  age INT,
+  is_admin BOOLEAN,
+  registration_date DATE,
+  col1 INT,
+  col2 LONG
+);
+
+-- COMMAND ----------
+
+
---
+++ Test Table.sql
@@ -0,0 +1,16 @@
+-- Databricks notebook source
+use catalog lenovo;
+use schema poc;
+
+-- COMMAND ----------
+
+INSERT INTO sample_table (id, name, age, is_admin, registration_date, col1, col2)
+VALUES (1, 'Will Wang', 37, true, '2024-04-22', 99, 999);
+
+-- COMMAND ----------
+
+select * from sample_table
+
+-- COMMAND ----------
+
+
